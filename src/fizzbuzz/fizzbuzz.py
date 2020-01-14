c94a774b75d7f6a1db1939d4f6ba372b29e926dc
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-14 01:56:50 +0000

    completed src/fizzbuzz/fizzbuzz.py to specification of tests

--- src/fizzbuzz/fizzbuzz.py
+++ src/fizzbuzz/fizzbuzz.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 for i in range(1, 101):
-	if   i % 3 == 0:
-		print("Fizz")
-	elif i % 5 == 0:
-		print("Buzz")
-	else:
-		print(i)
+	if i % 3 == 0:
+		print("Fizz", end="")
+
+	if i % 5 == 0:
+		print("Buzz", end="")
+
+	if i % 3 != 0 and i % 5 != 0:
+		print(i, end="")
+
+	print("\n", end="")
 
