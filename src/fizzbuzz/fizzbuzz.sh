dae4a8ebb04911607f9c37109f13face4b7187fb
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-15 03:57:31 +0000

    Make fizzbuzz.sh pass test_every_third_output_is_fizz

--- src/fizzbuzz/fizzbuzz.sh
+++ src/fizzbuzz/fizzbuzz.sh
@@ -2,6 +2,17 @@
 
 for i in $(awk 'BEGIN {for (j = 1; j <= 100; j++) print j}')
 do
-	echo "$i"
+	if   [ "$(($i%3))" -eq 0 ]
+	then
+		echo "Fizz"
+	elif [ "$(($i%5))" -eq 0 ]
+	then
+		echo "Buzz"
+	elif [ "$(($i%15))" -eq 0 ]
+	then
+		echo "FizzBuzz"
+	else
+		echo "$i"
+	fi
 done
 
