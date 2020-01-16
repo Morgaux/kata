17f26574005b892fd6490e91fad0dc5e4eaef0cd
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-16 00:01:07 +0000

    Completed remaining tests for haskel implementation

--- src/fizzbuzz/fizzbuzz.hs
+++ src/fizzbuzz/fizzbuzz.hs
@@ -1,4 +1,9 @@
 
+fizzes              = cycle [ "", "", "Fizz" ]
+buzzes              = cycle [ "", "", "", "", "Buzz" ]
+fizzes_and_buzzes   = [ f ++ b | (f, b) <- zip fizzes buzzes ]
+fizzbuzz            = [ if s == "" then show n else s | (n, s) <- zip [1..] fizzes_and_buzzes ]
+
 main :: IO ()
-main = putStrLn $ show $ take 100 [1..]
+main = putStrLn $ unlines $ take 100 fizzbuzz
 
