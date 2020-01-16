0fd81b84c34babbb30808526f3ab2c11f643002b
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-16 00:02:21 +0000

    Subtle error, in haskell unlines will leave a trailing new line, added init to remove the last \n

--- src/fizzbuzz/fizzbuzz.hs
+++ src/fizzbuzz/fizzbuzz.hs
@@ -5,5 +5,5 @@ fizzes_and_buzzes   = [ f ++ b | (f, b) <- zip fizzes buzzes ]
 fizzbuzz            = [ if s == "" then show n else s | (n, s) <- zip [1..] fizzes_and_buzzes ]
 
 main :: IO ()
-main = putStrLn $ unlines $ take 100 fizzbuzz
+main = putStrLn $ init $ unlines $ take 100 fizzbuzz
 
