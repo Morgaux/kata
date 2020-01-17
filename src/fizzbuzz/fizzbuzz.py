309d2327bb868ea77db5e7950241ef3ab20cae0b
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-17 03:09:47 +0000

    Simply the python implementation of the haskell implemnetation of fizzbuzz

--- src/fizzbuzz/fizzbuzz.py
+++ src/fizzbuzz/fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-def fizzes (n)            : return [ "Fizz" if i % 3 == 0 else "" for i in range(n) ]
-def buzzes (n)            : return [ "Buzz" if i % 5 == 0 else "" for i in range(n) ]
-def fizzes_and_buzzes (n) : return list(map(lambda f, b : f + b, fizzes(n), buzzes(n)))
-def fizzbuzz (n)          : return [ i if fizzes_and_buzzes(n + 1)[i] == "" else fizzes_and_buzzes(n + 1)[i] for i in range(1, n + 1) ]
+def fizzes (n)            : return "Fizz" if n % 3 == 0 else ""
+def buzzes (n)            : return "Buzz" if n % 5 == 0 else ""
+def fizzes_and_buzzes (n) : return fizzes(n) + buzzes(n)
+def fizzbuzz (n)          : return [ max(str(i), fizzes_and_buzzes(i)) for i in range(1, n + 1) ]
 
 for i in fizzbuzz(100):
 	print(i)
