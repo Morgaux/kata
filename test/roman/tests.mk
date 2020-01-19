d0ab4cf0447e417fdd8203c9d226f8bc34a794e9
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-19 23:53:36 +0000

    Added a roman numerals test

--- test/roman/tests.mk
+++ test/roman/tests.mk
@@ -2,9 +2,10 @@
 # _ROMAN_TESTS for a fizz buzz implementation
 #
 
-_ROMAN_TESTS = test_roman_file_is_executable               \
-               test_roman_file_exits_failure_without_stdin \
-               test_roman_file_exits_success_with_stdin
+_ROMAN_TESTS = test_roman_file_is_executable                         \
+               test_roman_file_exits_failure_without_stdin           \
+               test_roman_file_exits_success_with_stdin              \
+               test_roman_input_line_count_matches_output_line_count \
 
 _ROMAN_FILES = bin/roman/python_implementation
 
@@ -45,6 +46,20 @@ test_roman_file_exits_success_with_stdin:
 	@for FILE in ${_ROMAN_FILES} ; \
 	do \
 		if echo "" | "$$FILE" ; \
+		then \
+			echo "PASS" ; \
+		else \
+			echo "FAIL for "$$(basename "$$FILE")"" ; \
+		fi ; \
+	done
+	@echo " "
+
+test_roman_input_line_count_matches_output_line_count:
+	@echo "Starting: $@..." | sed 's/test_roman/test_that/g' | tr '_' ' '
+	@for FILE in ${_ROMAN_FILES} ; \
+	do \
+		_COUNT="$$RANDOM" ; \
+		if [ "$$(awk "BEGIN {for (j = 1; j <= $$_COUNT; j++) print j}" | "$$FILE" | wc -l)" = "$$_COUNT" ] ; \
 		then \
 			echo "PASS" ; \
 		else \
