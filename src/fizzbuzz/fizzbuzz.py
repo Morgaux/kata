2fbcffc2032d7bbe13173e66a33e74ab4e1f1200
Morgaux <morgaux@muninn.localdomain>
2020-01-19 20:13:06 +1300

    Fix python implementation

--- src/fizzbuzz/fizzbuzz.py
+++ src/fizzbuzz/fizzbuzz.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-def fizzes (n)            : return [ "Fizz" if i % 3 == 0 else "" for i in range(1, n + 1) ]
-def buzzes (n)            : return [ "Buzz" if i % 5 == 0 else "" for i in range(1, n + 1) ]
-def fizzes_and_buzzes (n) : return list(map(lambda f, b : f + b, fizzes(n), buzzes(n)))
-def fizzbuzz (n)          : return [ i if fizzes_and_buzzes(n)[i] == "" else fizzes_and_buzzes(n)[i] for i in range(1, n + 1) ]
+def fizzes (n)            : return "Fizz" if n % 3 == 0 else ""
+def buzzes (n)            : return "Buzz" if n % 5 == 0 else ""
+def fizzes_and_buzzes (n) : return fizzes(n) + buzzes(n)
+def fizzbuzz (n)          : return [ i if fizzes_and_buzzes(i) == "" else fizzes_and_buzzes(i) for i in range(1, n + 1) ]
 
-for i in fizzes_and_buzzes(100):
+for i in fizzbuzz(100):
 	print(i)
 
