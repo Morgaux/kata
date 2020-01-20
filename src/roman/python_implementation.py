32ff69404abf801c77b8481f0a905dd337282e64
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-20 01:48:17 +0000

    Completed fullfilment of current tests

--- src/roman/python_implementation.py
+++ src/roman/python_implementation.py
@@ -6,8 +6,22 @@ if sys.stdin.isatty():
 	exit(1) # there's no input
 
 def convert(number):
-	return str(number)
+	if number.isdigit():
+		number = int(number)
+	else:
+		return number
+
+	result = []
+
+	# thousands
+	result.append("M" * ((number // 1000) % 10))
+
+	result.append(["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM"][(number //  100) % 10])
+	result.append(["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "XC"][(number //   10) % 10])
+	result.append(["", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX"][(number //    1) % 10])
+
+	return "".join(result)
 
 for line in sys.stdin:
-	print(convert(line), end="")
+	print(convert(line.strip()))
 
