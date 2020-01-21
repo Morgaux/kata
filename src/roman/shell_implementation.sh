2d8a9457aa0ca3c878061180f1702cf9e5cab774
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-21 01:24:46 +0000

    Start shell implementation from scratch as it's starting to look like an awk implementation

--- src/roman/shell_implementation.sh
+++ src/roman/shell_implementation.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
-# vi: syntax=awk
 
-true + /; exec awk -f "$0" -- "$@"; exit; / {}
 
-BEGIN {
-	"tty" | getline r
-	if (r != "not a tty") exit 1
-}
-
-!/[0-9]+/ {
-	exit 1
-}
-
-/[0-9]+/ {
-	print $0
-}
 
