34f32eebca9c468be9d66295d23b1b6a664869a4
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-21 01:16:06 +0000

    Implement passes for tests regarding the  IO options

--- src/roman/shell_implementation.sh
+++ src/roman/shell_implementation.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+# vi: syntax=awk
 
+true + /; exec awk -f "$0" -- "$@"; exit; / {}
+
+BEGIN {
+	"tty" | getline r
+	if (r != "not a tty") exit 1
+}
+
+/[0-9]+/ {
+	print $0
+}
+
+!/[0-9]+/ {
+	exit 1
+}
 
