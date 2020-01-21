176ad83c35bf7e2d3757e182c5089a9afc5d9225
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-21 02:50:40 +0000

    Skip haskell implmentation for now

--- test/roman/tests.mk
+++ test/roman/tests.mk
@@ -13,9 +13,8 @@ _ROMAN_TESTS = test_roman_file_is_executable                         \
                test_roman_100_random_values_are_correct              \
                test_roman_correctly_converts_multiple_inputs
 
-_ROMAN_FILES = bin/roman/python_implementation  \
-               bin/roman/shell_implementation   \
-               bin/roman/haskell_implementation
+_ROMAN_FILES = bin/roman/python_implementation \
+               bin/roman/shell_implementation
 
 test_roman: ${_ROMAN_TESTS}
 	@echo "All roman tests completed..."
