682e4515822a9cebedee536a95ea95eade58f23a
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-21 02:32:59 +0000

    Complete implementation

--- src/roman/shell_implementation.sh
+++ src/roman/shell_implementation.sh
@@ -13,7 +13,27 @@ while IFS='$\n' read -r LINE; do
 
 	echo "$(yes "I" | head -n "${LINE}" | tr '\n' ' ' | sed 's/ //g')" | \
 		sed '\
-			s/IIIIIIIIII/X/g \
+			s/IIIIIIIIII/X/g; \
+			s/IIIIIIIII/IX/g; \
+			s/IIIIIIII/IIX/g; \
+			s/IIIIIII/VII/g;  \
+			s/IIIIII/VI/g;    \
+			s/IIIII/V/g;      \
+			s/IIII/IV/g;      \
+			s/XXXXXXXXXX/C/g; \
+			s/XXXXXXXXX/XC/g; \
+			s/XXXXXXXX/XXC/g; \
+			s/XXXXXXX/LXX/g;  \
+			s/XXXXXX/LX/g;    \
+			s/XXXXX/L/g;      \
+			s/XXXX/XL/g;      \
+			s/CCCCCCCCCC/M/g; \
+			s/CCCCCCCCC/CM/g; \
+			s/CCCCCCCC/CCM/g; \
+			s/CCCCCCC/DCC/g;  \
+			s/CCCCCC/DC/g;    \
+			s/CCCCC/D/g;      \
+			s/CCCC/CD/g;      \
 		' # end sed call
 done
 
