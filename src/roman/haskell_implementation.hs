f8b653e5a469c962f4563dac54de8c6eb9321c41
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-22 22:06:13 +0000

    Interweave zeroAsDefault and romans functions

--- src/roman/haskell_implementation.hs
+++ src/roman/haskell_implementation.hs
@@ -1,24 +1,21 @@
-import Text.Read
+import Data.Maybe
 import Control.Monad
+import Text.Read
 
-zeroAsDefault :: Maybe Int -> Int
-zeroAsDefault mx = case mx of
-                        Nothing -> 0
-                        Just x -> x
-
-roman :: Int -> [Char]
-roman x
-    | x >= 1000 = "M" ++ roman (x - 1000)
-    | x >=  100 = ([ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM" ] !! ((x `div` 100) `mod` 10)) ++ roman (x `mod` 100)
-    | x >=   10 = ([ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "XC" ] !! ((x `div`  10) `mod` 10)) ++ roman (x `mod` 10)
+roman :: Maybe Int -> [Char]
+roman number
+    | x >= 1000 = "M" ++ roman (Just (x - 1000))
+    | x >=  100 = ([ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM" ] !! (mod (div x 100) 10)) ++ roman (Just (x `mod` 100))
+    | x >=   10 = ([ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "XC" ] !! (mod (div x  10) 10)) ++ roman (Just (x `mod`  10))
     | x >=    1 =  [ "", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX" ] !! x
     | otherwise = ""
+    where x = fromMaybe 0 number
 
 main :: IO ()
 main = do
        contents <- getContents
        forM_ (lines contents) $ \ line -> do
-             putStrLn $ case zeroAsDefault $ (readMaybe line :: Maybe Int) of
-                             0 -> line
-                             x -> roman x
+             putStrLn $ case roman $ (readMaybe line :: Maybe Int) of
+                             ""      -> line
+                             numeral -> numeral
 
