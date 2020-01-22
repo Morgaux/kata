489e5aa04c2e1fa2fc5ff12674e7cd17bc4334a3
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-22 22:37:29 +0000

    Tighten and clean up

--- src/roman/haskell_implementation.hs
+++ src/roman/haskell_implementation.hs
@@ -5,16 +5,14 @@ import Text.Read
 roman :: Maybe Int -> [Char]
 roman number
     | x >= 1000 = "M" ++ roman (Just (x - 1000))
-    | x >     0 = (
-                      (case base of               -- look up table
-                          100 -> [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM" ]
-                          10  -> [ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "XC" ]
-                          _   -> [ "", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX" ]
-                      ) !! (
-                          (x `div` base) `mod` 10  -- index to search for
-                      )
-                  ) ++ roman (Just (x `mod` base)) -- recurs on remainder
-    | otherwise = ""                               -- catch negatives and nothing
+    | x >     0 = ((case base of                -- look up table
+                         100 -> [ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM" ]
+                         10  -> [ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "XC" ]
+                         _   -> [ "", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX" ]
+                  ) !!
+                  ((x `div` base) `mod` 10)) ++ -- index to search for
+                  roman (Just (x `mod` base))   -- recurs on remainder
+    | otherwise = ""                            -- catch negatives and nothing
     where x    = fromMaybe 0 number
           base =      if x >= 100 then 100
                  else if x >=  10 then  10
