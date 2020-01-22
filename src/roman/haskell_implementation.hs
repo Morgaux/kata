f9f1566b0d726b0c41aeed7ea3f08be51a8a06db
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-22 22:11:15 +0000

    Make ones match existing pattern to make reducing easier

--- src/roman/haskell_implementation.hs
+++ src/roman/haskell_implementation.hs
@@ -7,7 +7,7 @@ roman number
     | x >= 1000 = "M" ++ roman (Just (x - 1000))
     | x >=  100 = ([ "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM" ] !! (mod (div x 100) 10)) ++ roman (Just (x `mod` 100))
     | x >=   10 = ([ "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "XC" ] !! (mod (div x  10) 10)) ++ roman (Just (x `mod`  10))
-    | x >=    1 =  [ "", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX" ] !! x
+    | x >=    1 = ([ "", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX" ] !! (mod (div x   1) 10)) ++ roman (Just (x `mod`   1))
     | otherwise = ""
     where x = fromMaybe 0 number
 
