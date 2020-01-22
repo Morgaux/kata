c1112e15aec6a54a5649e75f68ddc150afd3e036
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-22 20:20:51 +0000

    Added a roman numeral conversion function, correctly converts thosands and hundreds

--- src/roman/haskell_implementation.hs
+++ src/roman/haskell_implementation.hs
@@ -6,11 +6,26 @@ zeroAsDefault mx = case mx of
                         Nothing -> 0
                         Just x -> x
 
+roman :: Int -> [Char]
+roman x
+    | x >= 1000 = "M" ++ roman (x - 1000)
+    | x >=  100 = case (x `div` 100) `mod` 10 of
+                       1 -> "C"
+                       2 -> "CC"
+                       3 -> "CCC"
+                       4 -> "CD"
+                       5 -> "D"
+                       6 -> "DC"
+                       7 -> "DCC"
+                       8 -> "CCM"
+                       9 -> "CM"
+    | otherwise = show x
+
 main :: IO ()
 main = do
        contents <- getContents
        forM_ (lines contents) $ \ line -> do
              putStrLn $ case zeroAsDefault $ (readMaybe line :: Maybe Int) of
                              0 -> line
-                             x -> show x
+                             x -> roman x
 
