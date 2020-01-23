064940db20f9295a7b0202a4799884a6b39190e8
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-23 21:59:23 +0000

    change fizzzes_and_buzzes to strings, this makes extension better.

--- src/fizzbuzz/haskell_implementation.hs
+++ src/fizzbuzz/haskell_implementation.hs
@@ -1,8 +1,8 @@
 
-fizzes              = cycle [ "", "", "Fizz"         ]
-buzzes              = cycle [ "", "", "", "", "Buzz" ]
-fizzes_and_buzzes   = zipWith (++) fizzes buzzes
-fizzbuzz            = [ max s $ show n | (n, s) <- zip [1..] fizzes_and_buzzes ]
+fizzes   = cycle [ "", "", "Fizz"         ]
+buzzes   = cycle [ "", "", "", "", "Buzz" ]
+strings  = zipWith (++) fizzes buzzes
+fizzbuzz = [ max s $ show n | (n, s) <- zip [1..] strings ]
 
 main :: IO ()
 main = putStrLn $ init $ unlines $ take 100 fizzbuzz
