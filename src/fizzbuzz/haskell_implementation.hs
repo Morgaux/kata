1a9dc8d98e3ad2b5ed36be34c9ce39126f4c350c
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-23 21:49:41 +0000

    Clean up haskell implementation

--- src/fizzbuzz/haskell_implementation.hs
+++ src/fizzbuzz/haskell_implementation.hs
@@ -1,8 +1,8 @@
 
-fizzes              = cycle [ "", "", "Fizz" ]
+fizzes              = cycle [ "", "", "Fizz"         ]
 buzzes              = cycle [ "", "", "", "", "Buzz" ]
 fizzes_and_buzzes   = [ f ++ b | (f, b) <- zip fizzes buzzes ]
-fizzbuzz            = [ if s == "" then show n else s | (n, s) <- zip [1..] fizzes_and_buzzes ]
+fizzbuzz            = [ max s $ show n | (n, s) <- zip [1..] fizzes_and_buzzes ]
 
 main :: IO ()
 main = putStrLn $ init $ unlines $ take 100 fizzbuzz
