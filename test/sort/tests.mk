4e9f9e2bc7b5fc883774aa76b0889a5db4324ba1
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-28 00:31:56 +0000

    Begin python implementation of sort and add test to match input lines

--- test/sort/tests.mk
+++ test/sort/tests.mk
@@ -2,11 +2,13 @@
 # _SORT_TESTS for a sort implementation
 #
 
-_SORT_TESTS = test_sort_file_is_executable
+_SORT_TESTS = test_sort_file_is_executable                         \
+              test_sort_input_line_count_matches_output_line_count
 
-_SORT_FILES = 
+_SORT_FILES = bin/sort/python_implementation
 
 predicate_test_sort_file_is_executable = [ -x  $$FILE ]
+predicate_test_sort_input_line_count_matches_output_line_count = _COUNT="$$((RANDOM / 100))" ; [ "$$(awk "BEGIN {for (j = 1; j <= $$_COUNT; j++) print j}" | "$$FILE" | wc -l)" -eq "$$_COUNT" ] >/dev/null 2>&1 
 
 test_sort: ${_SORT_TESTS}
 	@echo All sort tests completed...
