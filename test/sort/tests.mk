ec1d46e1c3dc2ed79a29ee479fdc23e07127bca4
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-01-28 01:21:07 +0000

    Added additional sorting tests

--- test/sort/tests.mk
+++ test/sort/tests.mk
@@ -3,12 +3,27 @@
 #
 
 _SORT_TESTS = test_sort_file_is_executable                         \
-              test_sort_input_line_count_matches_output_line_count
+              test_sort_input_line_count_matches_output_line_count \
+              test_sort_single_letters                             \
+              test_sort_single_digits                              \
+              test_sort_multiletter                                \
+              test_sort_multidigit                                 \
+              test_sort_uppercase_first                            \
+              test_sort_zeros_correct                              \
+              test_sort_digits_before_letters
 
 _SORT_FILES = bin/sort/python_implementation
 
 predicate_test_sort_file_is_executable = [ -x  $$FILE ]
-predicate_test_sort_input_line_count_matches_output_line_count = _COUNT="$$((RANDOM / 100))" ; [ "$$(awk "BEGIN {for (j = 1; j <= $$_COUNT; j++) print j}" | "$$FILE" | wc -l)" -eq "$$_COUNT" ] >/dev/null 2>&1 
+predicate_test_sort_input_line_count_matches_output_line_count = _COUNT="$$((RANDOM / 100))" ; [ "$$(awk "BEGIN {for (j = 1; j <= $$_COUNT; j++) print j}" | "$$FILE" | wc -l)" -eq "$$_COUNT" ]
+predicate_test_sort_single_letters = [ "$$({ echo "c" ; echo "d" ; echo "b" ; echo "e" ; echo "a" ; } | "$$FILE" | sed 's/\n//g')" = "abcde" ]
+predicate_test_sort_single_digits = [ "$$({ echo "3" ; echo "4" ; echo "2" ; echo "5" ; echo "1" ; } | "$$FILE" | sed 's/\n//g')" = "12345" ]
+predicate_test_sort_multiletter = [ "$$({ echo "abc" ; echo "d" ; echo "aef" ; echo "gh" ; echo "lm" ; } | "$$FILE" | sed 's/\n//g')" = "abcaefdghlm" ]
+predicate_test_sort_multidigit = [ "$$({ echo "123" ; echo "45" ; echo "6" ; echo "78" ; echo "90" ; } | "$$FILE" | sed 's/\n//g')" = "6457890123" ]
+predicate_test_sort_uppercase_first = [ "$$({ echo "B" ; echo "a" ; echo "A" ; echo "C" ; echo "b" ; } | "$$FILE" | sed 's/\n//g')" = "AaBbC" ]
+predicate_test_sort_zeros_correct = [ "$$({ echo "1" ; echo "3" ; echo "0" ; echo "2" ; echo "0" ; } | "$$FILE" | sed 's/\n//g')" = "00123" ]
+predicate_test_sort_digits_before_letters = [ "$$({ echo "b" ; echo "5" ; echo "a" ; echo "0" ; echo "d" ; echo "c" ; } | "$$FILE" | sed 's/\n//g')" = "05abcd" ]
+
 
 test_sort: ${_SORT_TESTS}
 	@echo All sort tests completed...
