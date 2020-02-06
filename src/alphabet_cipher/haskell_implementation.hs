0b85cc1ce0b89324d1c0b2d7f9b153fe2de44b55
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-02-06 22:04:25 +0000

    Start the separate functions for the conversion types

--- src/alphabet_cipher/haskell_implementation.hs
+++ src/alphabet_cipher/haskell_implementation.hs
@@ -2,6 +2,12 @@
 -- haskell implementation of an alphabet cipher
 --
 
+encode key msg = msg
+
+decode key msg = msg
+
+decipher plain cipher = "key"
+
 main :: IO ()
 main = putStrLn "Hello World!"
 
