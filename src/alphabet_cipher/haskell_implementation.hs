c5ecbd61973f9bd56b862e9d040e6f6adbf839ab
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-02-06 22:22:33 +0000

    added cipher map

--- src/alphabet_cipher/haskell_implementation.hs
+++ src/alphabet_cipher/haskell_implementation.hs
@@ -2,6 +2,35 @@
 -- haskell implementation of an alphabet cipher
 --
 
+import qualified Data.Map as Map
+
+keyMap = Map.fromList [("a", "abcdefghijklmnopqrstuvwxyz")
+                      ,("b", "bcdefghijklmnopqrstuvwxyza")
+                      ,("c", "cdefghijklmnopqrstuvwxyzab")
+                      ,("d", "defghijklmnopqrstuvwxyzabc")
+                      ,("e", "efghijklmnopqrstuvwxyzabcd")
+                      ,("f", "fghijklmnopqrstuvwxyzabcde")
+                      ,("g", "ghijklmnopqrstuvwxyzabcdef")
+                      ,("h", "hijklmnopqrstuvwxyzabcdefg")
+                      ,("i", "ijklmnopqrstuvwxyzabcdefgh")
+                      ,("j", "jklmnopqrstuvwxyzabcdefghi")
+                      ,("k", "klmnopqrstuvwxyzabcdefghij")
+                      ,("l", "lmnopqrstuvwxyzabcdefghijk")
+                      ,("m", "mnopqrstuvwxyzabcdefghijkl")
+                      ,("n", "nopqrstuvwxyzabcdefghijklm")
+                      ,("o", "opqrstuvwxyzabcdefghijklmn")
+                      ,("p", "pqrstuvwxyzabcdefghijklmno")
+                      ,("q", "qrstuvwxyzabcdefghijklmnop")
+                      ,("r", "rstuvwxyzabcdefghijklmnopq")
+                      ,("s", "stuvwxyzabcdefghijklmnopqr")
+                      ,("t", "tuvwxyzabcdefghijklmnopqrs")
+                      ,("u", "uvwxyzabcdefghijklmnopqrst")
+                      ,("v", "vwxyzabcdefghijklmnopqrstu")
+                      ,("w", "wxyzabcdefghijklmnopqrstuv")
+                      ,("x", "xyzabcdefghijklmnopqrstuvw")
+                      ,("y", "yzabcdefghijklmnopqrstuvwx")
+                      ,("z", "zabcdefghijklmnopqrstuvwxy")]
+
 encode key msg = msg
 
 decode key msg = msg
