01ce4840bfb990f7e568cedc799ba95cd86a3b5b
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-02-10 13:56:42 +1300

    Adde color declarations file

---
+++ colors.mk
@@ -0,0 +1,15 @@
+#
+# Color declarations for colourful output (if supported)
+#
+
+RESET_OUTPUT = $$(tput sgr0)
+BLACK        = $$(tput setaf 0)
+RED          = $$(tput setaf 1)
+GREEN        = $$(tput setaf 2)
+YELLOW       = $$(tput setaf 3)
+BLUE         = $$(tput setaf 4)
+MAGENTA      = $$(tput setaf 5)
+CYAN         = $$(tput setaf 6)
+WHITE        = $$(tput setaf 7)
+BOLD         = $$(tput bold)
+
