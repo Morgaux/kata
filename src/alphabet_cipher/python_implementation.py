772a91df42d74aa7f672606f43a55c9d52497944
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-02-12 01:57:53 +0000

    Added skel for python

--- src/alphabet_cipher/python_implementation.py
+++ src/alphabet_cipher/python_implementation.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+import sys
+
+def encode(key, msg):
+    pass
+
+def decode(key, msg):
+    pass
+
+def decipher(plain, crypt):
+    pass
+
+def usage():
+    pass
+
+for line in sys.stdin:
+    line = line.strip()
+    options = line.split()
+    action = options[0]
+    if action == "encode":
+        encode("", "")
+    elif action == "decode":
+        decode("", "")
+    elif action == "":
+        decipher("", "")
+    else:
+        usage()
+        exit(1)
+
