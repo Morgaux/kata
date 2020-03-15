d7653eacee364f28c2c20b74fc31e1a068dcd1d7
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-03-15 20:20:57 +0000

    Added test for empty line case

--- test/palindrome/tests.mk
+++ test/palindrome/tests.mk
@@ -9,7 +9,8 @@
 KATA = "PALINDROME"
 
 TEST_CASES = test_file_is_executable                         \
-             test_input_line_count_matches_output_line_count
+             test_input_line_count_matches_output_line_count \
+             test_empty_line_fails
 
 TEST_FILES = bin/palindrome/python_implementation
 
@@ -17,4 +18,6 @@ all: message_before ${TEST_CASES}
 
 include test/tests.mk
 include builds.mk
+
+predicate_test_empty_line_fails = [ "$$(echo "" | $$FILE)" = "false" ]
 
