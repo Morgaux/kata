f2c9ce9a098e91d39ef7dc69715f71cd9865be56
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-03-29 11:10:40 +1300

    Completed Java implementation (in a typical java fashion)

--- src/fizzbuzz/java_implementation.java
+++ src/fizzbuzz/java_implementation.java
@@ -5,7 +5,35 @@
 class Kata {
     public static void main(String[] argv) {
         for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
+            switch (i % 15) {
+                case 0:
+                    System.out.println("FizzBuzz");
+                    break;
+
+                case 3:
+                case 6:
+                case 9:
+                case 12:
+                    System.out.println("Fizz");
+                    break;
+
+                case 5:
+                case 10:
+                    System.out.println("Buzz");
+                    break;
+
+                case 1:
+                case 2:
+                case 4:
+                case 7:
+                case 8:
+                case 11:
+                case 13:
+                case 14:
+                default:
+                    System.out.println(i);
+                    break;
+            }
         }
     }
 }
