6575ea41851aa7a57286eb99be9d2f7771330723
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-04 13:36:15 +1300

    Assume all years are leap years

--- src/leap_year/haskell_implementation.hs
+++ src/leap_year/haskell_implementation.hs
@@ -6,11 +6,16 @@ import Control.Monad
 import Data.Maybe
 import Text.Read
 
+-- IsLeap function {{{
+isLeap :: Int -> String
+isLeap year = "true"
+-- IsLeap function }}}
+
 -- Main function {{{
 main :: IO ()
 main = do contents <- getContents
           forM_ (lines contents) $ \ line -> do
-              putStrLn $ show $ fromMaybe 0 $ (readMaybe line :: Maybe Int)
+              putStrLn $ isLeap $ fromMaybe 0 $ (readMaybe line :: Maybe Int)
 -- Main function }}}
 
 
