3b70b74786750d319746375af2385bc9cccaf450
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-04 13:32:18 +1300

    Read line as number and print input number or zero

--- src/leap_year/haskell_implementation.hs
+++ src/leap_year/haskell_implementation.hs
@@ -3,12 +3,14 @@
 --
 
 import Control.Monad
+import Data.Maybe
+import Text.Read
 
 -- Main function {{{
 main :: IO ()
 main = do contents <- getContents
           forM_ (lines contents) $ \ line -> do
-              putStrLn line
+              putStrLn $ show $ fromMaybe 0 $ (readMaybe line :: Maybe Int)
 -- Main function }}}
 
 
