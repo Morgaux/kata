632c6378d9212d812ee908d93083863817c82d5c
Jin Meyer <4100409-kitkat1901@users.noreply.gitlab.com>
2020-04-06 21:44:08 +1200

    neater version

--- src/fizzbuzz/python_implementation.py
+++ src/fizzbuzz/python_implementation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 for i in range(1, 101, 1):
-        if i % 3 == 0 and not i % 15 == 0:
-            print("Fizz")
-        elif i % 5 == 0 and not i % 15 == 0:
-            print("Buzz")
-        elif i % 15 == 0:
+
+        if i % 15 == 0:
             print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
         else:
             print(i)
