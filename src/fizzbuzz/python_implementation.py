72a5b5126c1f24e64c61e257b9e28ce3394ec8f5
Jin Meyer <4100409-kitkat1901@users.noreply.gitlab.com>
2020-04-06 21:04:36 +1200

    Baby fiz

--- src/fizzbuzz/python_implementation.py
+++ src/fizzbuzz/python_implementation.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+for i in range(1, 101, 1):
+        if i % 3 == 0 and not i % 15 == 0:
+            print("Fizz")
+        elif i % 5 == 0 and not i % 15 == 0:
+            print("Buzz")
+        elif i % 15 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
