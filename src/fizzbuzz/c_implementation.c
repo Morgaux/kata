0beff64c8789f9ee19854cd08c3a9ba46ca95eae
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-12 14:31:04 +1200

    Naive solution

--- src/fizzbuzz/c_implementation.c
+++ src/fizzbuzz/c_implementation.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+#define WORD_LENGTH 256
+
 int main(char argv[], int argc) {
-	for (int i = 0; i < 100; i++) {
-		printf("%d\n", i + 1); // print accounting for off by one
+	for (int i = 1; i <= 100; i++) {
+		if (i % 15 == 0) {
+			printf("FizzBuzz\n");
+		} else if (i % 5 == 0) {
+			printf("Buzz\n");
+		} else if (i % 3 == 0) {
+			printf("Fizz\n");
+		} else {
+			printf("%d\n", i);
+		}
 	}
 
 	return 0;
