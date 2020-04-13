05a2e7eb89f15f9b4c8f70eb808455029c4a4c95
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-13 12:06:05 +1200

    Read print loop that breaks on non numeric input

--- src/roman_numerals/c_implementation.c
+++ src/roman_numerals/c_implementation.c
@@ -1,6 +1,25 @@
+#include <errno.h>
 #include <stdio.h>
 
 int main(char argv[], int argc) {
-	return 0;
+	int input, error;
+
+	/* Infinitely read stdin */
+	while (1) {
+		/* Read a number from stdin */
+		switch (scanf("%d", &input)) {
+		case 0:
+			errno = 1;
+		case EOF:
+			return errno;
+
+		default:
+			/* Print number back to stdout */
+			printf("%d\n", input);
+		}
+	}
+
+	/* generic error exit code */
+	return 1;
 }
 
