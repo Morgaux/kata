c8af2e7a6c8ebb7fabb81463ce33239ec04a25c4
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-16 13:10:12 +1200

    Make letter pool smaller and more readable

--- src/palindrome/python_implementation.py
+++ src/palindrome/python_implementation.py
@@ -6,32 +6,8 @@ def isPalindrome(line: str) -> bool:
     letters = []
     for letter in line.lower():
         if letter in [
-            "a",
-            "b",
-            "c",
-            "d",
-            "e",
-            "f",
-            "g",
-            "h",
-            "i",
-            "j",
-            "k",
-            "l",
-            "m",
-            "n",
-            "o",
-            "p",
-            "q",
-            "r",
-            "s",
-            "t",
-            "u",
-            "v",
-            "w",
-            "x",
-            "y",
-            "z",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+            "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
         ]:
             letters.append(letter)
     return letters == letters[::-1] and len(letters) > 0
