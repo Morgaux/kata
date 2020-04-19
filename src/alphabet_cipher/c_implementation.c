8fff1af3cadaac75ec7eefc7e6c6f9496ced73ce
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-20 05:36:35 +1200

    Added line reading skelleton and to lower function

--- src/alphabet_cipher/c_implementation.c
+++ src/alphabet_cipher/c_implementation.c
@@ -2,11 +2,64 @@
  * A C alphabet_cipher implementation
  */
 
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 512
+
+static char letters[] = {
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+};
+
+static char * encode(char * key, char * msg) {
+	return msg;
+}
+
+static char * decode(char * key, char * msg) {
+	return msg;
+}
+
+static char * decipher(char * plain, char * cipher) {
+	return plain;
+}
+
+static char * toLower(char * str) {
+	int i, len = strlen(str);
+	char * out = malloc(sizeof (char) * (len + 1));
+
+	for (i = 0; i < len; i++) {
+		*(out + i) = tolower(*(str + i));
+	}
+
+	return out;
+}
 
 int main(char argv[], int argc) {
-	printf("Hello World!\n");
+	size_t line_length;
+	char * line_string = NULL, key, msg, plain, cipher;
 
-	return 0;
+	while (1) {
+		switch (getline(&line_string, &line_length, stdin)) {
+		case EINVAL:
+		case ENOMEM:
+		case -1:
+			if (errno != 0) {
+				/* error due to actually error, not EOF */
+				fprintf(stderr, "getline() returned exited with code %d.");
+			}
+
+			return errno; /* will be 0 on EOF */
+
+		default:
+			/* line read of EOF reached */
+			printf(toLower(line_string));
+		}
+	}
+
+	return 1; /* unreachable */
 }
 
