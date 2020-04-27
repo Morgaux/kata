b5b288f3a9dc818909ace6795a41f62cf1e89751
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-04-27 19:40:21 +1200

    Outline skeleton for cipher

--- src/alphabet_cipher/java_implementation.java
+++ src/alphabet_cipher/java_implementation.java
@@ -7,12 +7,32 @@
 import java.util.*;
 
 class Kata {
-    public static void main(String[] argv) {
+    private String[] alphabet = { // {{{
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    }; // }}}
+
+    private String encode(String key, String msg) { // {{{
+        return "";
+    } // }}}
+
+    private String decode(String key, String msg) { // {{{
+        return "";
+    } // }}}
+
+    private String decipher(String plain, String cipher) { // {{{
+        return "";
+    } // }}}
+
+    public static void main(String[] argv) { // {{{
         Scanner in = new Scanner(System.in);
+        String line;
 
         while (in.hasNext()) {
-            System.out.println(in.nextLine());
+            line = in.nextLine();
+            line.split("/\\s+/");
+            System.out.println(line);
         }
-    }
+    } // }}}
 }
 
