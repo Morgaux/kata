61e115808891dd0f96e4a9a424ac4bbee4751889
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-05-05 10:02:58 +1200

    Move global config var to the top of the file

--- src/alphabet_cipher/python_implementation.py
+++ src/alphabet_cipher/python_implementation.py
@@ -2,13 +2,13 @@
 
 import sys
 
+letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+
 def uncycle(string, n = 1):
     if string.startswith(string[n:]): # and string[1:n + 1] == string[n:][1:n + 1]:
         return string[:n]
     else:
         return uncycle(string, n + 1)
-
-letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
 def encode(key, msg):
     result = []
