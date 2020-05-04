3c6cd261b78303bf43d91636897d9a94c1eb2ef1
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-05-05 10:04:08 +1200

    Formate letter config to make more readable

--- src/alphabet_cipher/python_implementation.py
+++ src/alphabet_cipher/python_implementation.py
@@ -2,7 +2,10 @@
 
 import sys
 
-letters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
+letters = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
 
 def uncycle(string, n = 1):
     if string.startswith(string[n:]): # and string[1:n + 1] == string[n:][1:n + 1]:
