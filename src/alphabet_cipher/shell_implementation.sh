ff962af138bbdd3ea3314668aa06287dd2f7617f
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-05-05 07:50:56 +1200

    Added skel for shell implementation of alphabet cipher

--- src/alphabet_cipher/shell_implementation.sh
+++ src/alphabet_cipher/shell_implementation.sh
@@ -2,5 +2,51 @@
 
 # A POSIX  alphabet_cipher implementation
 
-echo "Hello World!"
+encode() { # {{{ TODO
+	echo "ERROR: Not yet implemented." 1>&2
+	exit 1
+} # }}}
+
+decode() { # {{{ TODO
+	echo "ERROR: Not yet implemented." 1>&2
+	exit 1
+} # }}}
+
+decipher() { # {{{ TODO
+	echo "ERROR: Not yet implemented." 1>&2
+	exit 1
+} # }}}
+
+parse() { # {{{ TODO
+	echo "ERROR: Not yet implemented." 1>&2
+	exit 1
+} # }}}
+
+main() { # {{{ $@:unused
+	while read LINE
+	do
+		ACTION="$(echo "$LINE" | awk '{print $1}')"
+		KEY="$(parse "key")"
+		MSG="$(parse "message")"
+		PLAINTEXT="$(parse "plaintext")"
+		CIPHERTEXT="$(parse "ciphertext")"
+
+		case "$ACTION" in
+			"encode")
+				encode "$KEY" "$MSG"
+			;;
+			"decode")
+				decode "$KEY" "$MSG"
+			;;
+			"decipher")
+				decipher "$PLAINTEXT" "$CIPHERTEXT"
+			;;
+			*)
+				echo "$LINE"
+			;;
+		esac
+	done
+} # }}}
+
+main $@
 
