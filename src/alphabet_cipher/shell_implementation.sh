b0554565ec1848d2d09d00db2571fdd098f3eb52
Morgaux <3158796-morgaux@users.noreply.gitlab.com>
2020-05-17 16:50:16 +1200

    General layout from awk implementation

--- src/alphabet_cipher/shell_implementation.sh
+++ src/alphabet_cipher/shell_implementation.sh
@@ -2,9 +2,59 @@
 
 # A POSIX  alphabet_cipher implementation
 
-encode() { # {{{ TODO
-	echo "ERROR: Not yet implemented." 1>&2
-	exit 1
+LETTERS="abcdefghijklmnopqrstuvwxyz"
+LETTER_COUNT="26"
+
+gen_seq() { # {{{ $1:n
+	awk 'BEGIN {for (i = 0; i < '"$1"'; i++) print i}'
+} # }}}
+
+length() { # {{{ $@:str
+	echo "$@" | wc -c
+} # }}}
+
+char_at() { # {{{ $1:str $2:index
+	echo "$1" | head -c "$(expr "$2" + 1)" | tail -c 1
+} # }}}
+
+index_of() { # {{{ $1:str $2:char
+	index="0"
+
+	for char in $(echo "$1" | sed 's/\(.\)/\1\n/g')
+	do
+		if [ "$(char_at "$char" 0)" = "$(char_at "$2" 0)" ]
+		then
+			echo "$index"
+		fi
+
+		index="$(expr "$index" + 1)"
+	done
+
+	echo "-1"
+} # }}}
+
+encode() { # {{{ $1:key $2:message
+	out=""
+
+	for i in $(gen_seq "$(length "$2")")
+	do
+		for j in $(gen_seq "$(length "$LETTERS")")
+		do
+			if [ "$(char_at "$alphabet" "$j")" = "$(char_at "$1" "$(expr "$i" % "$(length "$2")")")" ]
+			then
+				key_index="$j"
+			fi
+
+			if [ "$(char_at "$LETTERS" "$j")" = "$(char_at "$1" "$i")" ]
+			then
+				msg_index="$j"
+			fi
+		done
+
+		out="${out}$(char_at "$LETTERS" "$(expr "$key_index" + "$msg_index")")"
+	done
+
+	echo "$out"
 } # }}}
 
 decode() { # {{{ TODO
